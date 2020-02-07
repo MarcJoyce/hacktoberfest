8a9fe5ba0e870f1d537f0bfaf09f81a1fd591176
MarcJoyce <joyce501games@gmail.com>
2020-02-07 20:08:27 +0000

    Complete Marc Joyce additions

---
+++ scripts/hello_world_MarcJoyce.js
@@ -0,0 +1,6 @@
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: Marc Joyce
+// GITHUB: https://github.com/MarcJoyce
+
+console.log("Hello World!! Im Marc Joyce, an enthusiastic and determined Web developer.");
